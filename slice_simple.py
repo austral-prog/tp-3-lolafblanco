39ea963dedcf4c1c8db4295f66ea5fd48e659496
lolafblanco <162721872+lolafblanco@users.noreply.github.com>
2024-04-05 12:23:43 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,11 @@
 def slice_simple():
     texto = "Awesome"
+    def slice_simple():
+    text = input("")
+    print(text[4::2])
+
+    numbers = input("")
+    print(numbers[4::2])
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
